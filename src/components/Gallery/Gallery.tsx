d445969b37b6ca4fd8ee390ddd22874a344c5fc6
Ammar Ahmad <118152219+korsc@users.noreply.github.com>
2023-10-14 23:23:47 -0400

    add: more pictures to gallery fix: top padding for gallery modals

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 import Image1 from './STARoboticsPhotos/stagallery1.webp';
 import Image2 from './STARoboticsPhotos/stagallery2.webp';
-import Image3 from './STARoboticsPhotos/stagallery3.webp';
 import Image4 from './STARoboticsPhotos/stagallery4.webp';
-// import Image5 from './STARoboticsPhotos/stagallery5.webp';
+import Image5 from './STARoboticsPhotos/stagallery5.webp';
+import Image7 from './STARoboticsPhotos/stagallery7.webp';
+import Image8 from './STARoboticsPhotos/stagallery8.webp';
+import Image9 from './STARoboticsPhotos/stagallery9.webp';
+import Image10 from './STARoboticsPhotos/stagallery10.webp';
 import './gallery.css';
 
 const Gallery: React.FC = () => {
@@ -38,13 +41,18 @@ const Gallery: React.FC = () => {
 
   return (
     <section id='gallery' className='box-shadow-wrapper pb-2'>
-    <h3 className='flex justify-center pt-7 font-Khula text-transparent text-7xl bg-clip-text bg-gradient-to-r from-red-500 to-pink-600 relative mt-[3%]'>Gallery</h3>
+    <h3 className='flex justify-center pt-7 font-Khula text-transparent text-7xl bg-clip-text bg-gradient-to-r from-red-500 to-pink-600 relative mt-[1%] md:mb-7 mobile:mb-5'>Gallery</h3>
     <div className="row-pg mb-[1%]">
       {/* COLUMN 1 */}
       <div className="column-pg">
+      <img
+          className="myImages mobile:mb-3 rounded-2xl mb-2"
+          src={Image2} 
+          style={{position:'relative'}}
+        />
         <img
           className="myImages mobile:mb-3 rounded-2xl"
-          src={Image1} 
+          src={Image9} 
           style={{position:'relative'}}
         />
       </div>
@@ -52,8 +60,13 @@ const Gallery: React.FC = () => {
       {/* COLUMN 2 */}
       <div className="column-pg">
       <img
+          className="myImages mobile:mb-3 rounded-2xl mb-2"
+          src={Image8} 
+          style={{position:'relative'}}
+        />
+        <img
           className="myImages mobile:mb-3 rounded-2xl"
-          src={Image2} 
+          src={Image5} 
           style={{position:'relative'}}
         />
       </div>
@@ -61,8 +74,13 @@ const Gallery: React.FC = () => {
       {/* COLUMN 3 */}
       <div className="column-pg">
       <img
+          className="myImages mobile:mb-3 rounded-2xl mb-2"
+          src={Image7} 
+          style={{position:'relative'}}
+        />
+      <img
           className="myImages mobile:mb-3 rounded-2xl"
-          src={Image3} 
+          src={Image4} 
           style={{position:'relative'}}
         />
       </div>
@@ -70,8 +88,13 @@ const Gallery: React.FC = () => {
       {/* COLUMN 4 */}
       <div className="column-pg">
       <img
-          className="myImages mobile:mb-3 rounded-2xl"
-          src={Image4} 
+          className="myImages mobile:mb-3 rounded-2xl mb-2"
+          src={Image1} 
+          style={{position:'relative'}}
+        />
+        <img
+          className="myImages mobile:mb-3 rounded-2xl mb-2"
+          src={Image10} 
           style={{position:'relative'}}
         />
       </div>
